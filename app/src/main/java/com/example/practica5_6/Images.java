91737fe7072f4a30bc7cec3620ee5984e71d16f0
Joel30 <joel3.0cj@gmail.com>
2019-06-17 15:23:47 -0400

    ruta de imagen

--- app/src/main/java/com/example/practica5_6/Images.java
+++ app/src/main/java/com/example/practica5_6/Images.java
@@ -1,14 +1,15 @@
 package com.example.practica5_6;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.security.Key;
 
 public class Images extends AppCompatActivity {
 
@@ -33,11 +34,25 @@ public class Images extends AppCompatActivity {
         if (key == null){
             key = getIntent().getStringExtra("load");
             imageView.setImageURI(Uri.parse(key));
-            textView1.setText(key);
+            String path = getRealPathFromURI(this,Uri.parse(key));
+            textView1.setText(path);
         } else {
             Bitmap bitmap = BitmapFactory.decodeFile(key);
             imageView.setImageBitmap(bitmap);
             textView1.setText(key);
         }
+    }
+
+    public static String getRealPathFromURI(Context context, Uri contentURI) {
+        String result = null;
+        Cursor cursor = context.getContentResolver().query(contentURI,null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            int idx = cursor
+                    .getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
     }
 }
